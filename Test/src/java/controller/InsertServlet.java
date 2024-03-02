da033bf3adb199d0e22357080d77c7f76707766d
ManhHuy15 <huya3k52@gmail.com>
2024-03-02 13:56:34 +0700

    test2
    ádsad

--- Test/src/java/controller/InsertServlet.java
+++ Test/src/java/controller/InsertServlet.java
@@ -13,7 +13,6 @@ import entity.MyClass;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.sql.Date;
@@ -25,42 +24,8 @@ import java.util.ArrayList;
  */
 public class InsertServlet extends BaseAuthen {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet InsertServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet InsertServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+   
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response,Account account)
             throws ServletException, IOException {
         ClassAreaDBContext cadbc = new ClassAreaDBContext();
@@ -72,15 +37,7 @@ public class InsertServlet extends BaseAuthen {
         request.getRequestDispatcher("View/insert.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response,Account account)
             throws ServletException, IOException {
         Customer c = new Customer();
@@ -92,7 +49,7 @@ public class InsertServlet extends BaseAuthen {
         MyClass mc = new MyClass();
         mc.setId(Integer.parseInt(request.getParameter("cid")));
         c.setClas(mc);
-
+//ádsadsad
         Area a = new Area();
         a.setId(Integer.parseInt(request.getParameter("aid")));
         c.setArea(a);
